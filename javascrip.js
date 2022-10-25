bc8218464dc64e970aab6f63630a2eb97ccf73c9
irakli1i <113178329+irakli1i@users.noreply.github.com>
2022-10-25 06:55:28 -0700

    davaleba 14

---
+++ javascrip.js
@@ -0,0 +1,32 @@
+// var arr=[2,3,5,11,15,17,20];
+// for(var i=0; i < arr.length; i++){
+//     document.write(arr[i]+" ");
+// }
+
+// var arr=[15,53,22,198,10,28,16,70,33,951];
+// for(var i=0; i < arr.length; i++){
+//     if (arr[i]%2==1){
+//         document.write(arr[i]+" ");
+//     }
+// }
+
+// var strSum=prompt ("შეიყვანეთ თანხა",)
+// function display(){
+//     strSum;
+// }
+
+// function res(){
+//     display();
+//     if(strSum<20){
+//         document.write("იაფი"); 
+//     }
+//     else if(strSum>20 && strSum<50){
+//         document.write("საშუალო"); 
+//     }
+//     else{
+//         document.write("ძვირი"); 
+//     }
+
+// }
+
+// res();
